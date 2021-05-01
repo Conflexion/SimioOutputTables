8a0a35123e1d6c7e13d9d246da3b20bc58a2ffc3
slackcat <slackcat@slackcat.com>
2021-05-01 16:13:03 -0400

    Adding SQL and Simio-only output tables model. ToDo:  Add documentation

---
+++ SQL/Query All.sql
@@ -0,0 +1,8 @@
+/*
+SELECT * FROM Links;
+SELECT * FROM Objects;
+*/
+
+SELECT * FROM dbo.MainTable   ORDER BY IntPK
+SELECT * FROM dbo.DeleteTable ORDER BY IntPK
+SELECT * FROM dbo.AddTable    ORDER BY SourceIntPK
